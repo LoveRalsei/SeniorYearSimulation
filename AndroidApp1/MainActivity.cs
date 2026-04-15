915513d5d484e8ec64ef77af0cf2601987ad9c09
伦苫 坤 <Gowithralsei@hotmail.com>
2026-04-15 10:25:22 +0800

    1

--- AndroidApp1/MainActivity.cs
+++ AndroidApp1/MainActivity.cs
@@ -4,6 +4,7 @@ namespace AndroidApp1
     public class MainActivity : Activity
     {
         Student student = new Student("111",2,3,4,5,6,6,7,8,9,"1",1,"2",2,"3",3);
+        TextView propertiesTextView;
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -11,9 +12,17 @@ namespace AndroidApp1
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
-            TextView propertiesTextView = FindViewById<TextView>(Resource.Id.propertiesTextView);
+            propertiesTextView = FindViewById<TextView>(Resource.Id.propertiesTextView);
 
-            propertiesTextView.Text = "\t健康：" + student.health + "\t精力：" + student.energy + "\t幸福：" + student.happiness + "\n111";
+            RefreshPropertiesTextView();
+        }
+
+        protected void RefreshPropertiesTextView()
+        {
+            propertiesTextView?.Text = $"\t健康：{student.health}\t精力：{student.energy}\t心情：{student.happiness}\n" +
+                $"\t魅力：{student.charm}\t懒惰：{student.laziness}\t迷茫：{student.confusion}\n" +
+                $"\t语文：{student.chinese}\t数学：{student.math}\t英语：{student.english}\n" +
+                $"\t{student.crouse1Name}：{student.crouse1Grade}\t{student.crouse2Name}：{student.crouse2Grade}\t{student.crouse3Name}：{student.crouse3Grade}";
         }
     }
 }
--- AndroidApp1/Student.cs
+++ AndroidApp1/Student.cs
@@ -5,27 +5,27 @@ namespace AndroidApp1
     internal class Student
     {
         //属性
-        public string name { get; set;}
-        public int health { get; set;}
-        public int energy { get; set;}
-        public int happiness { get; set;}
-        public int charm { get; set;}
-        public int laziness { get; set;}
-        public int confusion { get; set;}
+        public string name { get; set; }
+        public int health { get; set; }
+        public int energy { get; set; }
+        public int happiness { get; set; }
+        public int charm { get; set; }
+        public int laziness { get; set; }
+        public int confusion { get; set; }
 
-        public int chinese { get; set;}
-        public int math { get; set;}
-        public int english { get; set;}
-        public string crouse1Name { get; set;}
-        public int crouse1Grade { get; set;}
-        public string crouse2Name { get; set;}
-        public int crouse2Grade { get; set;}
-        public string crouse3Name { get; set;}
-        public int crouse3Grade { get; set;}
+        public int chinese { get; set; }
+        public int math { get; set; }
+        public int english { get; set; }
+        public string crouse1Name { get; set; }
+        public int crouse1Grade { get; set; }
+        public string crouse2Name { get; set; }
+        public int crouse2Grade { get; set; }
+        public string crouse3Name { get; set; }
+        public int crouse3Grade { get; set; }
 
 
         public Student(string name, int health, int energy, int happiness, int charm, int laziness, int confusion, int chinese, int math, int english, string crouse1Name, int crouse1Grade, string crouse2Name, int crouse2Grade, string crouse3Name, int crouse3Grade)
-        { 
+        {
             this.name = name;
             this.health = health;
             this.energy = energy;
@@ -42,7 +42,50 @@ namespace AndroidApp1
             this.crouse2Name = crouse2Name;
             this.crouse2Grade = crouse2Grade;
             this.crouse3Name = crouse3Name;
-            this.crouse3Grade = crouse3Grade;            
+            this.crouse3Grade = crouse3Grade;
+        }
+
+        public void IncreaseProperty(string propertyName, int value)
+        {
+            switch (propertyName)
+            {
+                case "health":
+                    this.health += value;
+                    break;
+                case "energy":
+                    this.energy += value;
+                    break;
+                case "happiness":
+                    this.happiness += value;
+                    break;
+                case "charm":
+                    this.charm += value;
+                    break;
+                case "laziness":
+                    this.laziness += value;
+                    break;
+                case "confusion":
+                    this.confusion += value;
+                    break;
+                case "chinese":
+                    this.chinese += value;
+                    break;
+                case "math":
+                    this.math += value;
+                    break;
+                case "english":
+                    this.english += value;
+                    break;
+                case "crouse1Grade":
+                    this.crouse1Grade += value;
+                    break;
+                case "crouse2Grade":
+                    this.crouse2Grade += value;
+                    break;
+                case "crouse3Grade":
+                    this.crouse3Grade += value;
+                    break;
+            }
         }
     }
 }
